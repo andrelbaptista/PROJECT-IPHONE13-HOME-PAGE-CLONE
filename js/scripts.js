6b529a22c489344191e06d940918667afc67b401
Andre Baptista <122994174+andrelbaptista@users.noreply.github.com>
2024-06-03 17:34:04 -0300

    projeto finalizado

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+"use strict";
+const botaoSelecao = document.querySelectorAll("#cores li");
+const corTroca = document.querySelector("#imagem");
+
+console.log(botaoSelecao);
+console.log(corTroca);
+
+botaoSelecao.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    console.log(e);
+    botaoSelecao.forEach((btn) => {
+      btn.querySelector(".cor").classList.remove("selecao");
+    });
+    const botao = e.target;
+    const id = botao.getAttribute("id");
+    console.log(id);
+    botao.querySelector(".cor").classList.add("selecao");
+    corTroca.classList.add("troca");
+    corTroca.setAttribute("src",`img/iphone_${id}.jpg`);
+    setTimeout(() => {
+      corTroca.classList.toggle("troca");
+    },200)
+  });
+});
