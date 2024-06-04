cfee55d2289f64bae40fc1a65c009d478c459af0
Andre Baptista <122994174+andrelbaptista@users.noreply.github.com>
2024-06-04 12:19:57 -0300

    addicionado comentarios a console.log

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,19 @@
 const botaoSelecao = document.querySelectorAll("#cores li");
 const corTroca = document.querySelector("#imagem");
 
-console.log(botaoSelecao);
-console.log(corTroca);
+// console.log(botaoSelecao);
+// console.log(corTroca);
 
 botaoSelecao.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    console.log(e);
+    // console.log(e);
     botaoSelecao.forEach((btn) => {
       btn.querySelector(".cor").classList.remove("selecao");
     });
     const botao = e.target;
+    // console.log(`botao: ${botao}`)
     const id = botao.getAttribute("id");
-    console.log(id);
+    // console.log(id);
     botao.querySelector(".cor").classList.add("selecao");
     corTroca.classList.add("troca");
     corTroca.setAttribute("src",`img/iphone_${id}.jpg`);
